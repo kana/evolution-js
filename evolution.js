2a8fdeaa79c76a8c6fabd9d9cbc5f1b41dc23109
Kana Natsuno <dev@whileimautomaton.net>
2013-03-08 19:10:18 +0900

    Add plants

--- evolution.js
+++ evolution.js
@@ -2,5 +2,7 @@
   var MAP = {x: 0, y: 0, width: 100, height: 30};
   var JUNGLE = {x: 45, y: 10, width: 10, height: 10};
   var PLANET_ENERGY = 80;
+
+  var plants = {};  // (x, y) -> t
 })();
 // vim: expandtab softtabstop=2 shiftwidth=2

1486507f0f3c1ed9d947bcec68bd4cfad0a8377d
Kana Natsuno <dev@whileimautomaton.net>
2013-03-08 19:46:41 +0900

    Add REPRODUCTION_ENERGY

--- evolution.js
+++ evolution.js
@@ -2,6 +2,7 @@
   var MAP = {x: 0, y: 0, width: 100, height: 30};
   var JUNGLE = {x: 45, y: 10, width: 10, height: 10};
   var PLANET_ENERGY = 80;
+  var REPRODUCTION_ENERGY = 200;
 
   var plants = {};  // (x, y) -> t
 

4de848d1d67fc498fb34a6272e72392872672d9d
Kana Natsuno <dev@whileimautomaton.net>
2013-03-08 19:06:44 +0900

    Define constants

--- evolution.js
+++ evolution.js
@@ -1,3 +1,6 @@
 (function () {
+  var MAP = {x: 0, y: 0, width: 100, height: 30};
+  var JUNGLE = {x: 45, y: 10, width: 10, height: 10};
+  var PLANET_ENERGY = 80;
 })();
 // vim: expandtab softtabstop=2 shiftwidth=2
